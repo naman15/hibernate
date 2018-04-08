f36cd3f9be2db31108d24091d06d8b79e435db97
namangoyal15 <naman_goyal_94@yahoo.com>
2018-04-08 16:00:54 +0530

    Projections with JPA 2 and HQL

--- hibernate/src/main/java/hibernate/client/ManageEmployee.java
+++ hibernate/src/main/java/hibernate/client/ManageEmployee.java
@@ -3,7 +3,17 @@ package hibernate.client;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CompoundSelection;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -24,25 +34,82 @@ public class ManageEmployee {
       
       ManageEmployee ME = new ManageEmployee();
 
+      ME.deleteEmployees();
       /* Add few employee records in database */
-//      Integer empID1 = ME.addEmployee(1,"Zara", 1000);
-//      Integer empID2 = ME.addEmployee(2,"Daisy", 5000);
-//      Integer empID3 = ME.addEmployee(3,"John", 10000);
+      Integer empID1 = ME.addEmployee(1,"Zara", 10);
+      Integer empID2 = ME.addEmployee(2,"Daisy", 50);
+      Integer empID3 = ME.addEmployee(3,"John", 10);
 
       /* List down all the employees */
+      System.out.println("=================");
       ME.listEmployees();
 
       /* Update employee's records */
-      ME.updateEmployee(1, 5000);
+      ME.updateEmployee(empID1, 50);
 
       /* Delete an employee from the database */
-      ME.deleteEmployee(2);
+      ME.deleteEmployee(empID2);
+      
+      Integer empID4 = ME.addEmployee(4,"Johny", 10);
 
       /* List down new list of the employees */
+      System.out.println("=================");
       ME.listEmployees();
+      
+      /* List Total Salary sum*/
+      System.out.println("=================");
+      ME.printSumOfEmployeeSalaries();
    }
    
-   /* Method to CREATE an employee in the database */
+   private void printSumOfEmployeeSalaries() 
+   {
+	   Session session=factory.openSession();
+	   Transaction tx=null;
+	   try {
+	         tx = session.beginTransaction();
+	         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+	         //We want Integer result
+	         CriteriaQuery<Long> cr=criteriaBuilder.createQuery(Long.class);
+	         //The root does not need to match the type of the result of the query!
+	         Root<Employee> root=cr.from(Employee.class);
+	         CompoundSelection<Long> projection = criteriaBuilder.construct(Long.class, criteriaBuilder.sum(root.<Long>get("salary")));
+	         cr.select(projection);
+	         System.out.println(session.createQuery(cr).getSingleResult());
+	   }
+	   catch(HibernateException e) 
+	   {
+		   if(tx!=null)tx.rollback();
+		   e.printStackTrace();
+	   }
+	   finally 
+	   {
+		   session.close();
+	   }
+   }
+
+private void deleteEmployees() 
+   {
+	   Session session=factory.openSession();
+	   Transaction tx=null;
+	   try {
+	         tx = session.beginTransaction();
+	         String hql="Delete from Employee";
+	         TypedQuery<Employee> query=session.createQuery(hql);
+	         query.executeUpdate();
+	         
+	   }
+	   catch(HibernateException e) 
+	   {
+		   if(tx!=null)tx.rollback();
+		   e.printStackTrace();
+	   }
+	   finally 
+	   {
+		   session.close();
+	   }
+   }
+
+/* Method to CREATE an employee in the database */
    public Integer addEmployee(int id, String name, int salary){
       Session session = factory.openSession();
       Transaction tx = null;
